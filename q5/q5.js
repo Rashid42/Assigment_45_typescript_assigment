a6ecd2994dba2c44b89aa27031c5e69cfa28a722
Rashid42 <rashid.akram42@gmail.com>
2023-10-03 13:44:34 +0500

    q5 solved
    Quotes message saved.

---
+++ q5/q5.js
@@ -0,0 +1,4 @@
+"use strict";
+let famous_person = "Neil Armstrong";
+let messsage = `${famous_person} once said,"That's one small step for a man , a gaint leap for mankind."`;
+console.log(messsage);
---
+++ q5/q5.ts
@@ -0,0 +1,3 @@
+let famous_person:string = "Neil Armstrong" 
+let messsage : string= `${famous_person} once said,"That's one small step for a man , a gaint leap for mankind."`;
+console.log(messsage);
