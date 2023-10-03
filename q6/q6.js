e7b8634295a835d6067f80c6ee4a4f756c0060ce
Rashid42 <rashid.akram42@gmail.com>
2023-10-03 14:36:27 +0500

    q6 solved
    White spaces with change of line .

---
+++ q6/q6.js
@@ -0,0 +1,6 @@
+"use strict";
+let personName = "\t RASHID AKRMA \t";
+let statment = `My name is ${personName}and i am in batch 46 Q1. \n`;
+console.log(statment);
+let statment2 = "I am studing  in  \tPIAIC \t Following courses \n AI  \n Web 3.0 \n Metaverse .";
+console.log(statment2);
